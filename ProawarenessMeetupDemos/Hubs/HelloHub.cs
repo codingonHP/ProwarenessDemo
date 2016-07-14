0eba696455c6c309b31a6b6c65e96e44171306a3
vishal anand <vishal_24_anand@hotmail.com>
2016-07-14 12:31:21 +0530

    Added counter and hello world in signalR and client counter hub
    updated signal 2.0 to support breaking changes of jquery 3.x
    Added hub to demonstrate hello world and client counter.

---
+++ ProawarenessMeetupDemos/Hubs/HelloHub.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ProawarenessMeetupDemos.Hubs
+{
+    [HubName("HelloWorld")]
+    public class HelloHub : Hub
+    {
+        private static int ClientsCount { get; set; }
+        private static readonly object Lock = new object();
+
+        public override Task OnConnected()
+        {
+            lock (Lock)
+            {
+                ++ClientsCount;
+            }
+
+            UpdateConnectedClientsCount();
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            lock (Lock)
+            {
+                --ClientsCount;
+            }
+            UpdateConnectedClientsCount();
+            return base.OnDisconnected(stopCalled);
+        }
+
+        private void UpdateConnectedClientsCount()
+        {
+            Clients.All.updateClientsConnectedCount(ClientsCount);
+        }
+
+        public void SayHelloWorld()
+        {
+            string message = "hello world";
+            Clients.All.sayHello(message);
+        }
+
+       
+    }
+
+    public class HelloPersistance : PersistentConnection
+    {
+        
+    }
+}
