e3a7a59afb3c37b8818af27dfbb1fef25234d03a
vishal anand <vishal_24_anand@hotmail.com>
2016-08-19 16:01:53 +0530

    Simple user tracking application in signalR

---
+++ MySite/Hubs/TrackerHub.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using System;
+
+namespace MySite.Hubs
+{
+    public class TrackerHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            string data = $"<b>{DateTime.Now}</b> : Conn ID : { Context.ConnectionId } =>User : {Context.User.Identity.Name}, URL: { Context.Request.Url }, Agent : {Context.Request.Headers["USER-AGENT"]}";
+            Clients.All.trackClientsInfo(data);
+            return base.OnConnected();
+        }
+    }
+}
---
+++ MySite/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Cors;
+
+[assembly: OwinStartup(typeof(MySite.Startup))]
+
+namespace MySite
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/realtime", (action) =>
+            {
+                action.UseCors(CorsOptions.AllowAll);
+                action.RunSignalR(new Microsoft.AspNet.SignalR.HubConfiguration {
+                    EnableJSONP = true,
+                    EnableJavaScriptProxies = true
+                });
+            });
+        }
+    }
+}
